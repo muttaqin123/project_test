bc013ed9aa93853ec40cfe6f9796099e40e5c7dd
muttaqin123 <muttaqin0708@gmail.com>
2022-09-29 00:05:06 +0700

    Menambahkan datatables ke tampilan transaksi

--- resources/views/dashboard/menu/transaksi.blade.php
+++ resources/views/dashboard/menu/transaksi.blade.php
@@ -2,11 +2,13 @@
 
 @section('container')
 
-  <div class="container">
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs5/dt-1.12.1/datatables.min.css"/>
+
+  {{-- <div class="container">
     <div class="search">
         <input type="search" name="search" id="search" placeholder="search" class="form-control mt-3">
     </div>
-  </div>
+  </div> --}}
 
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">{{ $title_table }}</h1>        
@@ -18,10 +20,48 @@
       </div>
     @endif
     
-    <div class="table-responsive col-lg-12">      
+    <div class="table-responsive col-lg-12 mb-3">      
       <a href="/transaksi/create" class="btn btn-primary">Tambah Transaksi</a>      
-      <p>
-        <form action="/urut" method="post">
+    </div>
+    
+    <div class="containerr">
+      <table id ="myTable" class="table table-stripped">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nama Barang</th>
+            <th scope="col">Terjual</th>
+            <th scope="col">Transaksi</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach ($transaksi as $transaksii)
+          <tr>          
+            <td>{{ $transaksii->id }}</td>
+            <td>{{ $transaksii->nama_barang }}</td>
+            <td>{{ $transaksii->terjual }}</td>
+            <td>{{ $transaksii->tanggal_transaksi }}</td>
+            <td>
+              <a href="/transaksi/{{ $transaksii->id }}/edit" class="badge bg-warning"><span data-feather="edit"></span></a>
+              <form action="/transaksi/{{ $transaksii->id }}" method="post" class="d-inline"> 
+                @method('delete')
+                @csrf
+                <button class="badge bg-danger border-0" onclick="return confirm('Anda yakin akan menghapus data ini?')">
+                  <span data-feather="x-circle"></span>
+                </button>
+              </form>
+            </td>
+          </tr>
+          @endforeach     
+        </tbody>
+      </table>
+    </div>
+
+    {{-- <div class="table-responsive col-lg-12">       --}}
+      {{-- <a href="/transaksi/create" class="btn btn-primary">Tambah Transaksi</a>       --}}
+      {{-- <p> --}}
+        {{-- <form action="/urut" method="post">
           @csrf
           <div class="mt-3">
             Urut Berdasarkan Nama =  
@@ -35,10 +75,10 @@
               margin: 4px 10px;cursor: pointer; background-color:rgb(224, 215, 215);">
               Terapkan</button>
           </div>
-        </form>
-      </p>
+        </form> --}}
+      {{-- </p> --}}
 
-      <form action="/uruttanggal" method="post">
+      {{-- <form action="/uruttanggal" method="post">
         @csrf
         <div class="mt-1">
           <p>Urut Berdasarkan Tanggal =  
@@ -53,9 +93,9 @@
             Terapkan</button>
           </p>
         </div>
-      </form>
+      </form> --}}
 
-      <form action="/urutjual" method="post">
+      {{-- <form action="/urutjual" method="post">
         @csrf
         <div class="mt-1">
           <p>Urut Berdasarkan Jumlah Terjual =  
@@ -64,18 +104,15 @@
               <option value="menaik">Menaik</option>
               <option value="menurun">Menurun</option>
             </select>
-            {{-- Pilih Rentang Waktu
-            <input type="date" name="awal" id="awal"> -
-            <input type="date" name="akhir" id="akhir"> --}}
             <button type="submit" style="border: 1px solid; color: black; padding: 1px 20px;
             text-align: center;text-decoration: none; display: inline-block;font-size: 16px;
             margin: 4px 10px;cursor: pointer; background-color:rgb(224, 215, 215);">
             Terapkan</button>
           </p>
         </div>
-      </form>
+      </form> --}}
 
-        <table class="table table-striped table-sm">
+        {{-- <table class="table table-striped table-sm">
           <thead>
             <tr>
               <th scope="col">#</th>
@@ -109,34 +146,40 @@
 
           <tbody id="Content" class="searchdata"></tbody>
 
-        </table>
-      </div>
+        </table> --}}
+      {{-- </div> --}}
     
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+      <script type="text/javascript" src="https://cdn.datatables.net/v/bs5/dt-1.12.1/datatables.min.js"></script>
       <script type="text/javascript">
-        $('#search').on('keyup', function(){
-          // alert('hello');
-          $value=$(this).val();
-          // console.log('tolo');
-          if($value){
-            $('.alldata').hide();
-            $('.searchdata').show();
-          }else{
-            $('.alldata').show();
-            $('.searchdata').hide();
-          }
+      //   $('#search').on('keyup', function(){
+      //     // alert('hello');
+      //     $value=$(this).val();
+      //     // console.log('tolo');
+      //     if($value){
+      //       $('.alldata').hide();
+      //       $('.searchdata').show();
+      //     }else{
+      //       $('.alldata').show();
+      //       $('.searchdata').hide();
+      //     }
           
-          $.ajax({          
-            type : 'get',
-            url : '{{URL::to('searchhh')}}',
-            data:{'search':$value},
+      //     $.ajax({          
+      //       type : 'get',
+      //       url : '{{URL::to('searchhh')}}',
+      //       data:{'search':$value},
           
-            success:function(data){
-              // console.log(data);
-              $('#Content').html(data);
-            }
-          });
-      })
+      //       success:function(data){
+      //         // console.log(data);
+      //         $('#Content').html(data);
+      //       }
+      //     });
+      // })
+      </script>
+      <script>
+        $(document).ready( function () {
+          $('#myTable').DataTable();
+        } );
       </script>
 
 @endsection
